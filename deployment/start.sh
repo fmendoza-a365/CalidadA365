6c41080b058f598f21246d7d71f180362c3a8299
Fernando Mendoza <fmendoza@a365.com>
2026-01-02 08:05:08 -0500

    Add: Seeders to Dockerfile deployment script

--- deployment/start.sh
+++ deployment/start.sh
@@ -3,6 +3,11 @@
 # Run Migrations
 php artisan migrate --force || true
 
+# Run Seeders
+php artisan db:seed --class=RoleSeeder --force || true
+php artisan db:seed --class=PermissionSeeder --force || true
+php artisan db:seed --class=AdminSeeder --force || true
+
 # Cache Config
 php artisan config:cache
 php artisan route:cache
