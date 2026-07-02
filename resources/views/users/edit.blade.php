3c128eec59bc69b8f5e9b3188a4214d61d95d9a3
fmendoza-a365 <fmendoza@a365.com.pe>
2026-07-02 14:51:38 -0500

    feat: show validation errors in edit user form

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -15,6 +15,28 @@
                 @csrf
                 @method('PUT')
                 
+                @if ($errors->any())
+                    <div class="mb-6 p-4 bg-red-50 dark:bg-red-900/20 border-l-4 border-red-500 rounded-r-lg">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"/>
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <h3 class="text-sm font-medium text-red-800 dark:text-red-200">
+                                    Por favor corrige los siguientes errores:
+                                </h3>
+                                <ul class="mt-2 list-disc list-inside text-sm text-red-700 dark:text-red-300 space-y-1">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                
                 <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
                     
                     <!-- Sidebar / Photo -->
